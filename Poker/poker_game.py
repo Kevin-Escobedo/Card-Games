bfad5293f474b7b72317fa6309d8dc61e04303c3
Kevin C. Escobedo <escobedo001@gmail.com>
2019-08-05 13:55:13 -0700

    Fixed bug in check_high_card()

--- Poker/poker_game.py
+++ Poker/poker_game.py
@@ -178,8 +178,7 @@ class PokerGame:
     def check_high_card(self, hand) -> int:
         '''Checks cards for highest card #10'''
         cards = sorted(hand + self.flop + self.turn + self.river, key = lambda c: int(c))
-        values = [card.value for card in cards]
-        return max(values)
+        return max([int(card) for card in cards])
 
 
 if __name__ == "__main__":
